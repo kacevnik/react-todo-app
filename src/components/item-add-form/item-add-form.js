c49fd48474322999e32e8b014b38640d291f908f
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-11-24 12:57:41 +0300

    added new items

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,50 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
-    render(){
+
+    state = {
+        value: ''
+    }
+
+    onChangeValue = (e) => {
+        const val = e.target.value;
+        this.setState(({value}) => {
+            return {
+                value: val
+            }
+        });
+    }
+
+    onSubmitItem = (e) => {
+        e.preventDefault();
+        const text = this.state.value;
+        if(!text) return;
         const {onAddItem} = this.props;
+        onAddItem(text);
+        this.setState(({value}) => {
+            return {
+                value: ''
+            }
+        });
+    }
+
+    render(){
         return(
-            <div className="item-add-form">
+            <form
+                className="item-add-form d-flex"
+                onSubmit={this.onSubmitItem}
+            >
+                <input
+                    type="text"
+                    placeholder="What to do?"
+                    className="form-control"
+                    value={this.state.value}
+                    onChange={this.onChangeValue}
+                />
                 <button
                     className="btn btn-outline-secondary"
-                    onClick={() => onAddItem('Hello World')}
                 >Add Item</button>
-            </div>
+            </form>
         );
     }
 }
